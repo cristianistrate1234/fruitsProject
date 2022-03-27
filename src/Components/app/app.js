51f7b128a1ee1a4f1deb050b3683ede0ab4b0368
Cristi2002 <cristianistrate1234@gmail.com>
2022-03-27 20:38:19 +0300

    Am creat partea mea(Cristi)

--- src/Components/app/app.js
+++ src/Components/app/app.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import CardBlog from "../blog-info";
 
 class App extends Component {
     render() {
         return (
             <div>
-
+            <CardBlog />
             </div>
         );
     }
