2ffc82316bcb927d34ef6388e17bd1739454fea7
Cristi2002 <cristianistrate1234@gmail.com>
2022-03-27 21:24:07 +0300

    Cristi

--- src/Components/app/app.js
+++ src/Components/app/app.js
@@ -2,14 +2,16 @@ import React, {Component} from 'react';
 import ThumbContainer from "../thumb-container/thumb-container";
 import CardBlog from "../blog-info";
 import ContainerSearch from "../container-Search-Info/container-NavBarMenu-Search";
+
 class App extends Component {
     render() {
         return (
             <div>
-            <CardBlog />
-              <ContainerSearch />
-          <ThumbContainer/>
+                <ContainerSearch/>
+                <ThumbContainer/>
+                <CardBlog/>
             </div>
         );
     }
+}
 export default App;
