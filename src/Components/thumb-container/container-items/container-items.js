9eea94e0da69b5958edeaebb41c68b3fdcbbb1af
Kalepson <kalepson@gmail.com>
2022-03-27 22:54:49 +0300

    Am corectat erorile dela mihai

--- src/Components/thumb-container/container-items/container-items.js
+++ src/Components/thumb-container/container-items/container-items.js
@@ -7,7 +7,7 @@ function ContainerItems() {
     return (
         <div className={`containerItems`}>
             {bottomContainerItems.map((content, index) => {
-                return (<div key={index} className={`juice`} style={{backgroundImage: `url(${content.imgBackground})`}}>
+                return (<div key={index} className={`juice`} style={{ backgroundImage: `url(${content.imgBackground})`}}>
                     <div className={'text'}>
                         <p className={'pText'}>{content.ptext}</p>
                         <p className={'pText1'}>{content.ptext1}</p>
@@ -30,8 +30,8 @@ export function TopThumb() {
             {topContainerItems.map((content, index) => {
                 return (
                     <div className={'topContainer1'} key={index}>
-                        <img src={content.img} alt=""/>
-                        <button>{content.buttonTitle}</button>
+                        <img className={"img-ContainerItem"} src={content.img} alt=""/>
+                        <button className={"btn-ContainerItem"}>{content.buttonTitle}</button>
                     </div>
                 )
             })}
